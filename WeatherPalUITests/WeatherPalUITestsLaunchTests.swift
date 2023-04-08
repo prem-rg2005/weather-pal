b47a44cc6e49e3e481c254610e20defceb829709
Prem Shankar <prem.rg2005@gmail.com>
2023-04-08 13:09:54 +0800

    Initial Commit

---
+++ WeatherPalUITests/WeatherPalUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  WeatherPalUITestsLaunchTests.swift
+//  WeatherPalUITests
+//
+//  Created by Prem Shankar Rajagopalan Nayar on 8/4/23.
+//
+
+import XCTest
+
+class WeatherPalUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
